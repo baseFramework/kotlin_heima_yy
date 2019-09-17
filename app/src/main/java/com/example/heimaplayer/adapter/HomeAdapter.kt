aa1299fb57c426c3bc59d030dc330f2ac62b5a36
wurh <ro87630872@126.com>
2019-09-17 18:56:47 +0800

    【P248】上拉加载更多
    上拉加载更多

--- app/src/main/java/com/example/heimaplayer/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/heimaplayer/adapter/HomeAdapter.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.heimaplayer.widget.HomeItemView
+import com.example.heimaplayer.widget.LoadMoreView
 import com.itheima.player.model.bean.HomeItemBean
 
 class HomeAdapter:RecyclerView.Adapter<HomeAdapter.HomeHolder>(){
@@ -17,19 +18,45 @@ class HomeAdapter:RecyclerView.Adapter<HomeAdapter.HomeHolder>(){
         notifyDataSetChanged()
     }
 
+    // 加载更多数据
+    fun loadMore(list:List<HomeItemBean>){
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class HomeHolder(itemView: View):RecyclerView.ViewHolder(itemView){
 
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if(position == list.size){
+            return 1
+        }else{
+            return 0
+        }
+    }
+
+
     override fun getItemCount(): Int {
-        return  list.size
+        return  list.size + 1
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeHolder {
-        return HomeHolder(HomeItemView(parent?.context))
+        if(viewType ==1){
+            // 最后一条
+            return HomeHolder(LoadMoreView(parent.context))
+        }else{
+            // 普通条目
+            return HomeHolder(HomeItemView(parent.context))
+        }
+
     }
 
     override fun onBindViewHolder(holder: HomeHolder, position: Int) {
+
+        // 如果是最后一条 就不需要再来刷新view
+        if(position == list.size) return
+
         // 条目数据
         val data = list.get(position)
 
--- app/src/main/java/com/example/heimaplayer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/heimaplayer/ui/fragment/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.heimaplayer.ui.fragment
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.heimaplayer.R
 import com.example.heimaplayer.adapter.HomeAdapter
 import com.example.heimaplayer.base.BaseFragment
@@ -31,10 +32,85 @@ class HomeFragment:BaseFragment() {
 
 
         recycleView.adapter = adapter
+
+        // 监听列表滑动
+        recycleView.addOnScrollListener(object:RecyclerView.OnScrollListener(){
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+//               when(newState){
+//                   RecyclerView.SCROLL_STATE_IDLE->{
+//                       println("idel")
+//                   }
+//                   RecyclerView.SCROLL_STATE_DRAGGING->{
+//                       println("drag")
+//                   }
+//                   RecyclerView.SCROLL_STATE_SETTLING->{
+//                       println("set")
+//                   }
+//               }
+
+                if(newState == RecyclerView.SCROLL_STATE_IDLE){
+                    // 是否最后一条显示
+                    val layoutManager = recyclerView.layoutManager
+                    if(layoutManager is LinearLayoutManager){
+                        val manager:LinearLayoutManager = layoutManager
+                        val lastPosition = manager.findLastVisibleItemPosition()
+                        if(lastPosition == adapter.itemCount -1){
+                            //最后一条已经显示了
+                            loadMore(adapter.itemCount-1)
+                        }
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                println("on scrolled dx=$dx dy =$dy")
+            }
+        })
     }
 
     override  fun initData(){
         loadDatas()
+    }
+
+    // 加载更多数据
+    private  fun loadMore(offset:Int){
+        val path = URLProviderUtils.getHomeUrl(offset,20)
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(path)
+            .get()
+            .build()
+        client.newCall(request).enqueue(object :Callback{
+
+            // 子线程调用
+            override fun onFailure(call: Call, e: IOException) {
+                println("获取数据失败:")
+                println(e)
+                myToast("获取数据失败")
+
+            }
+
+            //子线程调用
+            override fun onResponse(call: Call, response: Response) {
+                myToast("获取数据成功")
+                val result =  response.body()?.string()
+                val gson = Gson()
+                val home = gson.fromJson(result, HomeBean::class.java)
+                val list = home.items
+                //gson.fromJson<>()
+//                val list = gson.fromJson<List<HomeItemBean>>(result,object: TypeToken<List<HomeItemBean>>(){}.type)
+                println("获取数据成功:" + list.size)
+
+                // 刷新列表
+                ThreadUtil.runOnMainThread(object :Runnable{
+                    override fun run() {
+                        adapter.loadMore(list)
+                    }
+                })
+
+            }
+        })
     }
 
     private  fun loadDatas(){
